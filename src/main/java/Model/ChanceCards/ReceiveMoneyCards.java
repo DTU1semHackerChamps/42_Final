7e40e00c68291987393e9e2548d0f8a9e87cf511
Stig <s186333@student.dtu.dk>
2021-01-07 16:25:58 +0100

    added ReceiveMoneyCards code

--- src/main/java/Model/ChanceCards/ReceiveMoneyCards.java
+++ src/main/java/Model/ChanceCards/ReceiveMoneyCards.java
@@ -1,4 +1,20 @@
 package Model.ChanceCards;
 
-public class ReceiveMoneyCards {
+import gui_main.GUI;
+
+
+
+public class ReceiveMoneyCards extends ChanceCard {
+    private int balanceChange;
+
+    public ReceiveMoneyCards(String cardDescription, Tile[] tiles, Player[] players, Player currentPlayer, GUI gui, int balanceChange) {
+        super(cardDescription, tiles, players, currentPlayer, gui);
+        this.balanceChange = balanceChange;
+    }
+
+    @Override
+    public void useChanceCard() {
+        gui.showMessage(cardDescription);
+        currentPlayer.addBalance(balanceChange);
+    }
 }
