a0eacb7d2e126ca5ccc1adad3a3c9a0af08b19e5
Stig <s186333@student.dtu.dk>
2021-01-07 15:38:06 +0100

    added abstract class ChanceCard

--- src/main/java/Model/ChanceCards/ChanceCard.java
+++ src/main/java/Model/ChanceCards/ChanceCard.java
@@ -1,4 +1,25 @@
 package Model.ChanceCards;
 
-public class ChanceCard {
+import gui_main.GUI;
+import Model.Player;
+import Controller.TileController;
+
+public abstract class ChanceCard {
+    protected String cardDescription;
+    protected Tile[] tiles;
+    protected Player[] players;
+    protected Player currentPlayer;
+    protected GUI gui;
+
+    public ChanceCard(String cardDescription, Tile[] tiles, Player[] players, Player currentPlayer, GUI gui) {
+        this.tiles = tiles;
+        this.cardDescription = cardDescription;
+        this.players = players;
+        this.currentPlayer = currentPlayer;
+        this.gui = gui;
+    }
+
+    public abstract void useChanceCard();
+
+    public abstract void setCurrentPlayer(Player currentPlayer);
 }
