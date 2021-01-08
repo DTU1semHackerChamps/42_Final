a4c79e7b251d1b90c2e67a5661f5f183a799c4b4
Stig <s186333@student.dtu.dk>
2021-01-08 09:56:22 +0100

    added chanceCard shuffle and drawcard methode

--- src/main/java/Controller/ChanceCardController.java
+++ src/main/java/Controller/ChanceCardController.java
@@ -1,4 +1,43 @@
 package Controller;
 
+import java.util.Random;
+
 public class ChanceCardController {
+
+
+
+
+    public static void cardShuffle(ChanceCard[] cardList){
+
+        int index;
+        ChanceCard temp;
+        Random random = new Random();
+        for (int i = cardList.length - 1; i >= 0; i--)
+        {
+            index = random.nextInt(i+1);
+            temp = cardList[index];
+            cardList[index] = cardList[i];
+            cardList[i] = temp;
+            System.out.println(cardList[i]);
+        }
+
+    }
+
+    public static int drawCard(int index, ChanceCard[] chanceCards, Player currentPlayer){
+        for (int i = 0; i < chanceCards.length; i++) {
+            chanceCards[i].setCurrentPlayer(currentPlayer);
+        }
+
+        index++;
+
+        chanceCards[index].useChanceCard();
+
+
+        if(index > 20){
+            index = 0;
+        }
+
+        return index;
+    }
+
 }
