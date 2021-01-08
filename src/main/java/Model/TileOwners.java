6c1b0e14fa808bd75bceab8f8307ea6aea33f395
Machnuud <Albert88->
2021-01-08 14:03:34 +0100

    Features added to TileOwners.java

--- src/main/java/Model/TileOwners.java
+++ src/main/java/Model/TileOwners.java
@@ -2,6 +2,30 @@ package Model;
 
 public class TileOwners {
 
-    public
+    private int arrayLength;
+    private int[] tileOwners;
 
+    public TileOwners (int length) {
+
+        arrayLength = length;
+        tileOwners = new int[arrayLength];
+    }
+
+    public int[] getTileOwners() {
+        return tileOwners;
+    }
+
+    public int getTileOwner(int position) {
+        int tileOwner;
+        tileOwner = tileOwners[position];
+        return tileOwner;
+    }
+
+    public void setTileOwners(int[] tileOwners) {
+        this.tileOwners = tileOwners;
+    }
+
+    public void setTileOwner(int position, int owner) {
+        tileOwners[position] = owner;
+    }
 }
