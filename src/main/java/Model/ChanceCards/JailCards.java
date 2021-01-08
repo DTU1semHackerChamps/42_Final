672e89a6b81fd7014ace29718ae27433d715fb36
Stig <s186333@student.dtu.dk>
2021-01-08 09:44:34 +0100

    added jail card

--- src/main/java/Model/ChanceCards/JailCards.java
+++ src/main/java/Model/ChanceCards/JailCards.java
@@ -1,4 +1,24 @@
 package Model.ChanceCards;
+import gui_main.GUI;
+import Model.Player;
+import Controller.TileController;
 
-public class JailCards {
+public class JailCard extends ChanceCard {
+
+    public JailCard(String cardDescription, Tile[] tiles, Player[] players, Player currentPlayer, GUI gui) {
+        super(cardDescription, tiles, players, currentPlayer, gui);
+    }
+
+    @Override
+    public void useChanceCard() {
+        gui.showMessage(cardDescription);
+        currentPlayer.setJailCard(true);
+    }
+
+    @Override
+    public void setCurrentPlayer(Player currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+
+
 }
