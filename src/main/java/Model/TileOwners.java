5d29861f336ec7d2796666f0e689674cb6ecabb5
Emil Trangeled <emilbfh@hotmail.com>
2021-01-09 15:43:20 +0100

    Added some functionality to BuildableTile

--- src/main/java/Model/TileOwners.java
+++ src/main/java/Model/TileOwners.java
@@ -2,29 +2,48 @@ package Model;
 
 public class TileOwners {
 
-    private int[] tileOwners;
+    private int[][] tileOwners;
 
-    public TileOwners (int length) {
-        tileOwners = new int[length];
+    public TileOwners () {
+        tileOwners = new int[40][3];
     }
 
-    public int[] getTileOwners() {
+    public int[][] getTileOwners() {
         return tileOwners;
     }
 
     public int getTileOwner(int position) {
         int tileOwner;
-        tileOwner = tileOwners[position];
+        tileOwner = tileOwners[position][1];
         return tileOwner;
     }
 
-    public void setTileOwners(int[] tileOwners) {
+    public int getTileHouses(int position) {
+        int houses;
+        houses = tileOwners[position][2];
+        return houses;
+    }
+
+    public int getTileHotel(int position) {
+        int hotel;
+        hotel = tileOwners[position][3];
+        return hotel;
+    }
+
+    public void setTileOwners(int[][] tileOwners) {
         this.tileOwners = tileOwners;
     }
 
     public void setTileOwner(int position, int owner) {
-        tileOwners[position] = owner;
+        tileOwners[position][1] = owner;
     }
 
+    public void setTileHouses(int position, int houses){
+        tileOwners[position][2] = houses;
+    }
+
+    public void setTileHotel(int position, int hotel){
+        tileOwners[position][3] = hotel;
+    }
 
 }
