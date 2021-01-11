402c81d85f7b9a8984437fdd5063b4e7ab149352
Emil Trangeled <emilbfh@hotmail.com>
2021-01-11 15:04:47 +0100

    Added Specific colors to GUI cars

--- src/main/java/View/PlayerView.java
+++ src/main/java/View/PlayerView.java
@@ -21,15 +21,26 @@ public class PlayerView {
      * @param player The name displayed in the GUI by the score
      * @return
      */
-    public static GUI_Player[] displayAddPlayer(GUI gui, GUI_Field[] fields, Player[] player){
-
-
-        GUI_Player[] guiPlayerList = new GUI_Player[4];
+    public static GUI_Player[] displayAddPlayer(HashMap<String, String> stringList, GUI gui, GUI_Field[] fields, Player[] player){
+        int numOfPlayers = player.length;
+        GUI_Player[] guiPlayerList = new GUI_Player[numOfPlayers];
 
         for (int i = 0; i < player.length; i++) {
             GUI_Car carColor = new GUI_Car();
 
-            GUI_Player gui_Player = new GUI_Player(gui.getUserString("Skriv navn her: ") ,player[i].getBalance(), carColor);
+            switch (i) {
+                case 0: carColor.setPrimaryColor(Color.green); break;
+                case 1: carColor.setPrimaryColor(Color.red); break;
+                case 2: carColor.setPrimaryColor(Color.yellow); break;
+                case 3: carColor.setPrimaryColor(Color.black); break;
+                case 4: carColor.setPrimaryColor(Color.blue); break;
+                case 5: carColor.setPrimaryColor(Color.gray); break;
+
+            }
+
+            String name = gui.getUserString(stringList.get("writeNameMsg"));
+            GUI_Player gui_Player = new GUI_Player(name, player[i].getBalance(), carColor);
+
             gui.addPlayer(gui_Player);
             fields[0].setCar(gui_Player, true);
             guiPlayerList[i] = gui_Player;
