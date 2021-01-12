33bad396b532191dc18d76d5a7839b526f0dcba4
Zahed <zahedm45@gmail.com>
2021-01-12 11:02:23 +0100

    buyHouse method created, but not done yet..

--- src/main/java/Model/TilesData/Buy_property/buyProperty.java
+++ src/main/java/Model/TilesData/Buy_property/buyProperty.java
@@ -1,4 +1,28 @@
 package Model.TilesData.Buy_property;
 
+import Model.BuildableTilePrices;
+import Model.Player;
+import Model.Tiles.BuildableTile;
+
 public class buyProperty {
+
+    public boolean buyHouse(BuildableTilePrices[] data, Player[] player, int tileNum) {
+
+
+        switch (tileNum) {
+            case 1: case 3: case 6: case 8: case 9: case 11: case 13: case 14:
+            case 16: case 18: case 19: case 21: case 23: case 24: case 26:
+            case 27: case 29: case 31: case 32: case 34: case 37: case 39:
+
+
+           data[tileNum].getPriceOfHouse();
+
+
+                for (int i = 0; i < player.length; i++) {
+                    player[i] =
+
+                }
+        }
+
+    }
 }
