b06724d3448caa8d46127e56dac81e6fdec2f5bd
Zahed <zahedm45@gmail.com>
2021-01-14 12:12:49 +0100

    made some changes..

--- src/main/java/Model/Tiles/ChanceCardTile.java
+++ src/main/java/Model/Tiles/ChanceCardTile.java
@@ -10,9 +10,19 @@ public class ChanceCardTile extends Tile{
 
     @Override
     public void executeTile() {
-        Dice dice = new Dice(0);
-        int SumOfFaceValue = dice.rollDice() + dice.rollDice();
-        currentPlayer.setPosition(currentPlayer.getPosition()+ SumOfFaceValue);
+
+        boolean isItChanceCard = true;
+        while (isItChanceCard) {
+            Dice dice = new Dice(0);
+            int SumOfFaceValue = dice.rollDice() + dice.rollDice();
+            currentPlayer.setPosition(currentPlayer.getPosition()+ SumOfFaceValue);
+            switch (currentPlayer.getPosition()) {
+                case 2: case 7: case 17:
+                case 22: case 33: case 36:
+                    break;
+                default: isItChanceCard = false;
+            }
+        }
 
     }
 }
