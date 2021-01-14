e94a7f3e604b2c2d3e2accbb332f0b11f5d2a145
Zahed <zahedm45@gmail.com>
2021-01-14 14:48:26 +0100

    commented on executeTile in TaxTile class.

--- src/main/java/Model/Tiles/TaxTile.java
+++ src/main/java/Model/Tiles/TaxTile.java
@@ -9,12 +9,15 @@ public class TaxTile extends Tile{
         super(currentPlayer, players);
     }
 
+    /**
+     * This method is responsible for taking either 4000 thousand or 10 percent of the player's balance, depends on the player's balance.
+     */
     @Override
     public void executeTile() {
 
         if (currentPlayer.getBalance() < 40000){
 
-            int tenPercentOfBalance = (int) (currentPlayer.getBalance() * 0.1);
+            int tenPercentOfBalance = (int) (currentPlayer.getBalance() * 0.10);
             currentPlayer.addBalance(-tenPercentOfBalance);
 
         } else currentPlayer.addBalance(-4000);
