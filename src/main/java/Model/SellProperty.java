b326abcea3c2aae3c753e03e286b7e59673385a5
Stig <s186333@student.dtu.dk>
2021-01-15 11:16:02 +0100

    added SellProperty and sellhouse methodes

--- src/main/java/Model/SellProperty.java
+++ src/main/java/Model/SellProperty.java
@@ -1,4 +1,32 @@
 package Model;
 
 public class SellProperty {
+
+    public static void sellHouse(BuildableTilePrices[] price, Player currentPlayer, int position, TileOwners tileOwners) {
+        int priceOfHouse;
+        priceOfHouse = price[position].getPriceOfHouse();
+
+
+        if (tileOwners.getTileHotel(position) == 1) {
+            tileOwners.setTileHotel(position, -1);
+            currentPlayer.addBalance((priceOfHouse * 5) / 2);
+        } else {
+            tileOwners.setTileHouses(position, -1);
+            currentPlayer.addBalance(priceOfHouse / 2);
+        }
+    }
+
+
+
+    public static boolean sellProperty(Player currentPlayer, int position, int bid, TileOwners tileOwners, Player[] players, int playerNum) {
+        boolean soldProperty = false;
+
+        if (players[playerNum].getBalance() > bid) {
+            players[playerNum].addBalance(-bid);
+            currentPlayer.addBalance(bid);
+            tileOwners.setTileOwner(position, playerNum);
+            soldProperty = true;
+        }
+        return soldProperty;
+    }
 }
