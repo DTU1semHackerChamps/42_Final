53347c296cdd3c559dd279c637dd20d80ca68af5
Stig <s186333@student.dtu.dk>
2021-01-15 16:25:39 +0100

    added playerNameList method

--- src/main/java/Model/PlayerNames.java
+++ src/main/java/Model/PlayerNames.java
@@ -1,21 +1,32 @@
 package Model;
 
+import gui_fields.GUI_Player;
+
 public class PlayerNames {
 
     private String[] names;
-    public PlayerNames(Player [] players) {
+
+    public PlayerNames(Player[] players) {
         this.names = new String[players.length];
     }
 
-    public void setPlayerName(int numOfPlayer, String name){
+    public void setPlayerName(int numOfPlayer, String name) {
         names[numOfPlayer] = name;
     }
 
-    public String getPlayerName(int numOfPlayer){
+    public String getPlayerName(int numOfPlayer) {
         String name = names[numOfPlayer];
         return name;
     }
 
+    public static String[] playerNameList(GUI_Player[] guiPlayer) {
 
+        String[] playerNames = new String[guiPlayer.length];
 
+        for (int i = 0; i < guiPlayer.length; i++) {
+            playerNames[i] = guiPlayer[i].getName();
+
+        }
+        return playerNames;
+    }
 }
--- src/main/java/View/PropertyOwnerView.java
+++ src/main/java/View/PropertyOwnerView.java
@@ -1,6 +1,7 @@
 package View;
 
 import Model.*;
+import gui_fields.GUI_Player;
 import gui_fields.GUI_Street;
 import gui_main.GUI;
 
@@ -21,6 +22,7 @@ public class PropertyOwnerView {
     /**
      * Menu which shows the available properties that contains buildings, and returns an integer value for the desired
      * location the player wants to sell a building on.
+     *
      * @param stringList
      * @param boardData
      * @param gui
@@ -46,33 +48,80 @@ public class PropertyOwnerView {
                 }
 
             }
+        }
+        // Filters out the empty strings
+        int k = 0;
+        String[] streetNames = new String[availableTiles];
+
+        for (int i = 0; i < boardData.length; i++) {
+            if (!tempStreetNames[i].equals(" ")) {
+                streetNames[k] = tempStreetNames[i];
+                k++;
             }
-            // Filters out the empty strings
-            int k = 0;
-            String[] streetNames = new String[availableTiles];
-
-            for (int i = 0; i < boardData.length; i++) {
-                if (!tempStreetNames[i].equals(" ")) {
-                    streetNames[k] = tempStreetNames[i];
-                    k++;
-                }
-            }
-
-            if (availableTiles == 0) {
-                gui.showMessage(stringList.get("noSellableBuildings"));
-            } else {
-
-                String propertyName = gui.getUserSelection(stringList.get("chooseSellableBuildings"), streetNames);
-
-                for (int i = 0; i < boardData.length; i++) {
-                    if (propertyName == boardData[i].getStreetName()) {
-                        housePosition = i;
-                        break;
-                    }
-                }
-            }
-        return housePosition;
         }
 
-    }
+        if (availableTiles == 0) {
+            gui.showMessage(stringList.get("noSellableBuildings"));
+        } else {
 
+            String propertyName = gui.getUserSelection(stringList.get("chooseSellableBuildings"), streetNames);
+
+            for (int i = 0; i < boardData.length; i++) {
+                if (propertyName == boardData[i].getStreetName()) {
+                    housePosition = i;
+                    break;
+                }
+            }
+        }
+        return housePosition;
+    }
+}
+
+// commented because of limited time. unsure if this feature can be implemented in time.
+// method for chooseing the property you want to sell, and selecting the player you want to sell to + the price.
+
+//    public static int chooseSellProperty (HashMap<String, String> stringList, GUIBoardData[] boardData, GUI gui, Player currentPlayer, TileOwners owners, PropertyGroup[] propertyGroups){
+//        int housePosition = -1;
+//        int availableTiles = 0;
+//        String[] tempStreetNames = new String[boardData.length];
+//        for (int i = 0; i < boardData.length; i++) {
+//            tempStreetNames[i] = " ";
+//            if (currentPlayer.getPlayerNum() == owners.getTileOwner(i) && PropertyGroup.hasGroup(owners, currentPlayer, i, propertyGroups)) {
+//                if ( (owners.getTileHotel(i) == 0) || (owners.getTileHotel(i) == 0) ){
+//                    tempStreetNames[i] = boardData[i].getStreetName();
+//                    availableTiles++;
+//                }
+//            }
+//        }
+//        // Filters out the empty strings
+//        int k = 0;
+//        String[] streetNames = new String[availableTiles];
+//
+//        for (int i = 0; i < boardData.length; i++) {
+//            if (!tempStreetNames[i].equals(" ")) {
+//                streetNames[k] = tempStreetNames[i];
+//                k++;
+//            }
+//        }
+//        if (availableTiles == 0) {
+//            gui.showMessage(stringList.get("noSellableProperties"));
+//        } else {
+//            String propertyName = gui.getUserSelection(stringList.get("chooseSellableProperties"), streetNames);
+//
+//            for (int i = 0; i < boardData.length; i++) {
+//                if (propertyName == boardData[i].getStreetName()) {
+//                    housePosition = i;
+//                    break;
+//                }
+//            }
+//        }
+//       return housePosition;
+//    }
+//
+//public static int chooseSellPlayer (HashMap<String, String> stringList, GUI gui, GUI_Player[] guiPlayer) {
+//
+//    String playerNames = gui.getUserSelection(stringList.get("choosePlayers"), PlayerNames.playerNameList(guiPlayer));
+//
+//}
+//    price = gui.getUserInteger(stringList.get("choosePlayers"));
+//
