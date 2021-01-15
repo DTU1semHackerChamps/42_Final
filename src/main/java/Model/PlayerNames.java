5575eb167c49c0b03a43b7283c1858060fa6ed5f
Djuncle <djuncledk@gmail.com>
2021-01-15 09:55:04 +0100

    Added comments in PlayerNames

--- src/main/java/Model/PlayerNames.java
+++ src/main/java/Model/PlayerNames.java
@@ -2,20 +2,22 @@ package Model;
 
 public class PlayerNames {
 
+    // String array containing all names
     private String[] names;
-    public PlayerNames(Player [] players) {
+
+    // Constructor for our PlayerNames class
+    public PlayerNames(Player[] players) {
         this.names = new String[players.length];
     }
 
+    // Setter method for setting the player name
     public void setPlayerName(int numOfPlayer, String name){
         names[numOfPlayer] = name;
     }
 
+    // Getter method for getting the player name
     public String getPlayerName(int numOfPlayer){
         String name = names[numOfPlayer];
         return name;
     }
-
-
-
 }
